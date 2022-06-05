53a9b86834eb78457b65eee7b5564f56994b494c
juanvacag <vacagonzalezjuan@gmail.com>
2022-06-05 10:12:42 +0200

    first commit

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NuevoPresupuesto = () => {
+  return (
+    <div className='contenedor-presupuesto contenedor sombra'>
+        
+        <form>
+
+        </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
