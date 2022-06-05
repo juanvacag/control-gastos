2bb6cef0c39ca871200e4b16effe0b198ec176b6
juanvacag <vacagonzalezjuan@gmail.com>
2022-06-05 11:01:33 +0200

    nueva atualizacion

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import {useState} from 'react'
+import Mensaje from './Mensaje'
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+
+  const [mensaje, setMensaje] = useState('');
+
+  const handlePresupuesto = (e) => {
+        e.preventDefault();
+        
+        if(!Number(presupuesto) || Number(presupuesto) < 0) {
+          setMensaje("No es un presupuesto válido");
+        } else {
+          setMensaje("Si es un presupuesto válido");
+        }
+  }
+
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
         
-        <form>
+        <form onSubmit={handlePresupuesto} className='formulario'>
+            <div className='campo'>
+              <label>Definir presupuesto</label>
 
+              <input
+                  className='nuevo-presupuesto'
+                  type="text"
+                  placeholder='Añade tu Presupuesto'
+                  value={presupuesto}
+                  onChange={(e) =>setPresupuesto(e.target.value)}
+              />
+
+              <input
+                type="submit"
+                value="Añadir"
+                />
+
+              {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+            </div>
         </form>
     </div>
   )
