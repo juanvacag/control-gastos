ea5f66b87aceadc42237d375e56e48cba2d4319d
juanvacag <vacagonzalezjuan@gmail.com>
2022-06-06 10:57:47 +0200

    nuevo gasto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
+import Modal from './components/Modal'
 
 function App() {
 
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [modal, setModal] = useState(false); 
+  const [animarModal, setAnimarModal] = useState(false);
+  
+  const handleNuevoGasto = () => {
+    //console.log("click para añadir un nuevo gasto");
+    setModal(true);
+
+    setTimeout(() => {
+        //console.log('animando formulario en un segundo');
+        setAnimarModal(true);
+    }, 500);
+  }
 
   return (
     <div>
@@ -14,6 +28,20 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+      {isValidPresupuesto && (
+            <div className='nuevo-gasto'>
+                <img
+                  src={IconoNuevoGasto}
+                  alt="icono nuevo gasto"
+                  onClick={handleNuevoGasto}
+                  />
+            </div>
+      )}
+      {modal && <Modal 
+                setModal={setModal}
+                animarModal={animarModal}
+                setAnimarModal={setAnimarModal}
+                />}
     </div>
   )
 }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,32 @@
+import {useState} from 'react'
+import CerrarBtn from '../img/cerrar.svg'
+
+const Modal = ({setModal, animarModal, setAnimarModal}) => {
+
+    const ocultarModal = () => {
+        //console.log("ocultando...");
+        setAnimarModal(false)
+
+        setTimeout(() => {
+            setModal(false)
+        }, 500);
+    }
+
+  return (
+    <div className="modal">
+        <div className='cerrar-modal'>
+            <img 
+                src={CerrarBtn}
+                alt="cerrar ventana modal"
+                onClick={ocultarModal}
+            />
+        </div>
+
+        <form className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
+            <legend>Nuevo Gasto</legend>
+        </form>
+    </div>
+  )
+}
+
+export default Modal
