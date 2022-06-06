642ed4772c59077d78c5100c52aba3e8260e35b8
juanvacag <vacagonzalezjuan@gmail.com>
2022-06-06 18:40:36 +0200

    video 113

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
-const ControlPresupuesto = ({presupuesto}) => {
+const ControlPresupuesto = ({presupuesto, gastos}) => {
+
+    const [disponible, setDisponible] = useState(0);
+    const [gastado, setGastado] = useState(0);
+
+    useEffect(() => {
+        const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0);  
+        const totalDisponible = presupuesto - totalGastado;
+
+        setGastado(totalGastado);
+        setDisponible(totalDisponible)
+    }, [gastos])
 
     const formatearCantidad = (cantidad) => {
         return cantidad.toLocaleString('es-ES', {
@@ -8,8 +19,6 @@ const ControlPresupuesto = ({presupuesto}) => {
             currency: 'EUR'
         })
     }
-
-
 
   return (
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
@@ -22,11 +31,11 @@ const ControlPresupuesto = ({presupuesto}) => {
             </p>
 
             <p>
-                <span>Disponible: </span>{formatearCantidad(0)}
+                <span>Disponible: </span>{formatearCantidad(disponible)}
             </p>
 
             <p>
-                <span>Gastado: </span>{formatearCantidad(0)}
+                <span>Gastado: </span>{formatearCantidad(gastado)}
             </p>
         </div>
     </div>
